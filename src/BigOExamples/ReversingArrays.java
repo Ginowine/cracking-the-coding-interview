ca0affc6810b7c5aacefee9a36d693bef0cbfc9a
ginowine <ginowine1@gmail.com>
2021-04-06 00:05:32 +0100

    printing unordered array pairs

---
+++ src/BigOExamples/ReversingArrays.java
@@ -0,0 +1,20 @@
+package BigOExamples;
+
+public class ReversingArrays {
+
+    static void reverseArray(int[] array){
+        for (int i = 0; i < array.length/2; i++){
+            int other = array.length - i - 1;
+            int temp = array[i];
+            array[i] = array[other];
+            array[other] = temp;
+
+            System.out.println(array[other]);
+        }
+    }
+
+    public static void main(String[] args){
+        int[] numbers = {2, 3, 4, 5};
+        reverseArray(numbers);
+    }
+}
