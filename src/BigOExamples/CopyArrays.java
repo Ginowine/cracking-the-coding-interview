29855af5d8196080efb5541d8f2003c83cb997d3
ginowine <ginowine1@gmail.com>
2021-04-15 16:51:55 +0100

    copy array from one to another array

---
+++ src/BigOExamples/CopyArrays.java
@@ -0,0 +1,24 @@
+package BigOExamples;
+
+public class CopyArrays {
+    public static void main(String[] args){
+
+        int[] oldArray = {1,3,5,7};
+
+        int[] newArray = new int[oldArray.length];
+
+        for (int i = 0; i < oldArray.length; i++){
+            newArray[i] = oldArray[i];
+        }
+        //newArray[0]++;
+
+        for (int i = 0; i < oldArray.length; i++){
+            System.out.print(oldArray[i] + " ");
+        }
+        System.out.print("\n");
+
+        for (int i = 0; i < newArray.length; i++){
+            System.out.print(newArray[i] + " ");
+        }
+    }
+}
