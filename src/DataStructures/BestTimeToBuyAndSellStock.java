b7c47901b5be498ed6feeae1ec2a62da3f82926e
ginowine <ginowine1@gmail.com>
2021-04-21 14:17:47 +0100

    best time to buy and sell stock

--- src/DataStructures/BestTimeToBuyAndSellStock.java
+++ src/DataStructures/BestTimeToBuyAndSellStock.java
@@ -8,18 +8,30 @@ public class BestTimeToBuyAndSellStock {
         System.out.println(result);
     }
 
-    public static int maxProfit(int[] prices) {
-        int maxProfit = 0;
-        int bestBuy = prices[0];
+//    public static int maxProfit(int[] prices) {
+//        int maxProfit = 0;
+//        int bestBuy = prices[0];
+//
+//        for(int price:prices){
+//            if(price < bestBuy){
+//                bestBuy = price;
+//            }else if(price - bestBuy > maxProfit){
+//                maxProfit = price - bestBuy;
+//            }
+//        }
+//
+//        return maxProfit;
+//    }
 
-        for(int price:prices){
-            if(price < bestBuy){
-                bestBuy = price;
-            }else if(price - bestBuy > maxProfit){
-                maxProfit = price - bestBuy;
+    public static int maxProfit(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
             }
         }
-
-        return maxProfit;
+        return maxprofit;
     }
 }
