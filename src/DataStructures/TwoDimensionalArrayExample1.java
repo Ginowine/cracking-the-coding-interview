a668d0a401d6913a63c353daae5b90a0bb577231
ginowine <ginowine1@gmail.com>
2021-04-21 09:50:18 +0100

    printing values in multidimensional array

--- src/DataStructures/TwoDimensionalArrayExample1.java
+++ src/DataStructures/TwoDimensionalArrayExample1.java
@@ -3,11 +3,11 @@ package DataStructures;
 public class TwoDimensionalArrayExample1 {
     public static void main(String[] args){
         int[][] numbers = {{1,2}, {3, 4}};
-
         for (int i = 0; i < 2; i++){
             for (int j = 0; j < 2; j++){
-                System.out.println("numbers[" + i + "][" + j + "] = " + numbers[i][j]);
+                System.out.print(numbers[i][j] + " ");
             }
+            System.out.println();
         }
     }
 }
