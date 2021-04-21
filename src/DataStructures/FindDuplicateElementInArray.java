256e37b5ebd30e4649780400d2af33a21aca5274
ginowine <ginowine1@gmail.com>
2021-04-21 06:58:39 +0100

    checking for duplicate elements in array

--- src/DataStructures/FindDuplicateElementInArray.java
+++ src/DataStructures/FindDuplicateElementInArray.java
@@ -1,10 +1,14 @@
 package DataStructures;
 
 public class FindDuplicateElementInArray {
+    public static void main(String[] args){
+        int[] numbers = {3, 4, 2};
+        boolean result = containsDuplicate(numbers);
 
-    FINISH THIS CODE OH
+        System.out.println(result);
+    }
 
-    public boolean containsDuplicate(int[] nums) {
+    public static boolean containsDuplicate(int[] nums) {
         for (int i = 0; i < nums.length; ++i) {
             for (int j = 0; j < i; ++j) {
                 if (nums[j] == nums[i]) return true;
