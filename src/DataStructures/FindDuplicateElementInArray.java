0dedc55c7ca50485430de74324de9099242c8130
ginowine <ginowine1@gmail.com>
2021-04-27 08:11:16 +0100

    weighted graph implementation using adjacency list

--- src/DataStructures/FindDuplicateElementInArray.java
+++ src/DataStructures/FindDuplicateElementInArray.java
@@ -2,16 +2,17 @@ package DataStructures;
 
 public class FindDuplicateElementInArray {
     public static void main(String[] args){
-        int[] numbers = {3, 4, 2};
+        int[] numbers = {9, 7, 2};
         boolean result = containsDuplicate(numbers);
 
         System.out.println(result);
     }
 
     public static boolean containsDuplicate(int[] nums) {
-        for (int i = 0; i < nums.length; ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[j] == nums[i]) return true;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] == nums[i])
+                    return true;
             }
         }
         return false;
